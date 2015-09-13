dc412af192235cf34e45bc57ee41c086144f7306
billowillo <masonstorms@gmail.com>
2015-09-12 19:38:22 -0600

    Still not correct.

--- Problem 2 - Even fibonacci numbers/evenfibbonaci.js
+++ Problem 2 - Even fibonacci numbers/evenfibbonaci.js
@@ -1,17 +1,17 @@
 function EvenFibbonaci() {
-	var sum = 0;
+
 	var x = 0;
 	var y = 0;
-	while (sum < 4000000) {
-		console.log("K");
-		y = x + (x + 1)
-		if (y % 2 == 0) {
-			sum += y
-
-			console.log(sum)
+	
+	for(var sum = 0; sum < 4000000;){
+		console.log(x);
+		y = x + (x + 1);
+		if(y % 2 == 0)
+		{
+			sum +=y;
 		}
 		x = y;
-
+		console.log(sum);
 	}
-
+	alert("done");
 }
